a1d3a37b3f1d55e48f98f97c9285f82d6b8f7855
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2023-11-08 14:01:18 +0100

    Code Cleanup: Add some doc

--- ConsoleApplication1/Board.cpp
+++ ConsoleApplication1/Board.cpp
@@ -8,16 +8,27 @@ using namespace std;
 Monster* playerMonsters;
 Monster* opponentMonsters;
 
+/*	Board()
+	The constructor for the board.
+	The position of the board is taken in and a delimiter space between each monster.
+*/
+
 Board::Board(sf::Vector2f boardPos, float delimiterSpace){
 	maxCapacity = 5;
+	radius = 80.0f;
+
 	playerMonsters   = new Monster[maxCapacity];
 	opponentMonsters = new Monster[maxCapacity];
-	
-	radius = 80.0f;
-	size = 0;
 	this->boardPos = boardPos;
 	this->delimiterSpace = radius * 2 + delimiterSpace;
+	
+	size = 0;
 }
+
+/*	addPlayerMonster
+*	A function that takes in a monster object and adds it to the array
+*	of player owned monsters.
+*/
 
 void Board::addPlayerMonster(Monster monster){
 	size++;
@@ -33,6 +44,11 @@ void Board::addOppponentMonster(){}
 bool Board::isFull(){
 	return size == maxCapacity;
 }
+
+/*	renderPlayerMonsters()
+	Iterate over every monster in the player array and render it.
+	Render with a delimiter that seperates monsters (delimiter is set in constructor).
+*/
 
 void Board::renderPlayerMonsters(sf::RenderWindow& window) {
 	for (int i = 0; i < size; i++) {
